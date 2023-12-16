4f3d9e9888ae50b1148463883fcdc64848f8704d
ELopes <overewer@gmail.com>
2023-12-16 08:08:23 -0300

    Okay Face Detect

--- main.cpp
+++ main.cpp
@@ -1,6 +1,102 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+ 
+using namespace std;
+using namespace cv;
+ 
+// Function for Face Detection
+void detectAndDraw( Mat& img, CascadeClassifier& cascade, 
+                CascadeClassifier& nestedCascade, double scale );
+string cascadeName, nestedCascadeName;
+ 
+int main( int argc, const char** argv )
+{
+    // VideoCapture class for playing video for which faces to be detected
+    VideoCapture capture; 
+    Mat frame, image;
+ 
+    // PreDefined trained XML classifiers with facial features
+    CascadeClassifier cascade, nestedCascade; 
+    double scale=1;
+ 
+    // Load classifiers from "opencv/data/haarcascades" directory 
+    // nestedCascade.load( "../../haarcascade_eye_tree_eyeglasses.xml" ) ;
+ 
+    // Change path before execution 
+    cascade.load( "haarcascade_frontalface_default.xml" ) ; 
+ 
+    // Start Video..1) 0 for WebCam 2) "Path to Video" for a Local Video
+    capture.open(0); 
+    if( capture.isOpened() )
+    {
+        // Capture frames from video and detect faces
+        cout << "Face Detection Started...." << endl;
+        while(1)
+        {
+            capture >> frame;
+            if( frame.empty() )
+                break;
+            
+            Mat frame1 = frame.clone();
+            detectAndDraw( frame1, cascade, nestedCascade, scale ); 
+            char c = (char)waitKey(10);
+         
+            // Press q to exit from window
+            if( c == 27 || c == 'q' || c == 'Q' ) 
+                break;
+        }
+    }
+    else
+        cout<<"Could not Open Camera";
+    return 0;
+}
+ 
+void detectAndDraw( Mat& img, CascadeClassifier& cascade,
+                    CascadeClassifier& nestedCascade,
+                    double scale)
+{
+    vector<Rect> faces, faces2;
+    Mat gray, smallImg;
+ 
+    cvtColor( img, gray, COLOR_BGR2GRAY ); // Convert to Gray Scale
+    double fx = 1 / scale;
 
-int main(int argc, char** argv){
-  return 0;
+    cv::Mat filtered;
+    cv::GaussianBlur(gray, filtered, cv::Size(121, 121), 2.0, 2.0);
+ 
+    // Resize the Grayscale Image 
+    resize( filtered, smallImg, Size(), fx, fx, INTER_LINEAR ); 
+    equalizeHist( smallImg, smallImg );
+ 
+    // Detect faces of different sizes using cascade classifier 
+    cascade.detectMultiScale( smallImg, faces, 1.1, 
+                            2, 0|CASCADE_SCALE_IMAGE, Size(24, 24) );
+ 
+    // Draw circles around the faces
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Rect r = faces[i];
+        Mat smallImgROI;
+        vector<Rect> nestedObjects;
+        Point center;
+        Scalar color = Scalar(255, 0, 0); // Color for Drawing tool
+        int radius;
+ 
+        double aspect_ratio = (double)r.width/r.height;
+        if( 0.92 < aspect_ratio && aspect_ratio < 1.08 )
+        {
+            center.x = round((r.x + r.width*0.5)*scale);
+            center.y = round((r.y + r.height*0.5)*scale);
+            radius = round((r.width + r.height)*0.25*scale);
+            circle( img, center, radius, color, 3, 8, 0 );
+        }
+        else
+            rectangle( img, Point(round(r.x*scale), round(r.y*scale)),
+                    Point(round((r.x + r.width-1)*scale), 
+                    round((r.y + r.height-1)*scale)), color, 3, 8, 0);
+ 
+    }
+ 
+    // Show Processed Image with detected faces
+    imshow( "Face Detection", img );
 }
